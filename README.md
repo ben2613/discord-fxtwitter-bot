6a5e88cca1e963f012433a3e271a64004c99f947
ben2613 <sosgyuki@gmail.com>
2022-05-01 10:56:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Another Discord fxtwitter bot
+
+This bot fix twitter links only when it detects that the tweet / tweet author is intentionally ignored by Discord Embedding Feature
---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client, Intents } = require("discord.js")
+require('dotenv').config()
+const urlPattern = /((?:http|ftp|https):\/\/(?:[\w_-]+(?:(?:\.[\w_-]+)+))(?:[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]))/
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.once('ready', () => { console.log('Ready!') })
+
+client.on('messageCreate', async (msg) => {
+    console.log(msg.content, msg.partial)
+    if (msg.content.includes("https://twitter.com/")) {
+        // embeds are empty at the beginning but appears after wait
+        // dunno why yet
+        setTimeout(() => {
+            let parts = msg.content.split(urlPattern)
+            let build = []
+
+            let needReplace = false
+            let allEmbedUrl = msg.embeds.reduce((prev, next) => prev.url + next.url, '')
+            parts.forEach((p) => {
+                if (isTweetURL(p) && !allEmbedUrl.includes(p)) {
+                    // check if it is in embed
+                    let url = new URL(p)
+                    url.hostname = 'fxtwitter.com'
+                    build.push(url.href)
+                    needReplace = true
+                } else {
+                    build.push(p)
+                }
+            })
+            if (needReplace) {
+                msg.delete()
+                msg.channel.send(`From ${msg.author} | ${build.join('')}`)
+            }
+        }, 2000)
+    }
+})
+
+client.login(process.env.TOKEN)
+
+function isTweetURL(url) {
+    return url.startsWith("https://twitter.com/") && url.includes('status')
+}
