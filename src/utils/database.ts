f6e18320c3b6cca523a9e86e92338a4fe02666f5
ben2613 <sosgyuki@gmail.com>
2022-05-08 22:50:57 +0100

    Add a small cache for keyv

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,31 +4,51 @@ export default class Database {
 
     SEPARATOR: string;
 
-    kv: Keyv<string, Record<string, unknown>>;
+    kv: Keyv;
+    cache: Map<string, unknown>;
 
     constructor(connstr: string | undefined) {
-        this.SEPARATOR = '-_-'
-        this.kv = new Keyv<string, Record<string, unknown>>(connstr)
+        this.SEPARATOR = '-_-';
+        this.kv = new Keyv(connstr);
+        this.cache = new Map<string, string>();
     }
 
-    set(guildId: string, k: string, v: string): Promise<true> {
-        return this.kv.set(guildId + this.SEPARATOR + k, v)
+    set(guildId: string, k: string, v: unknown): Promise<true> {
+        let key = guildId + this.SEPARATOR + k;
+        return this._set(key, v);
     }
 
-    get(guildId: string, k: string): Promise<string | undefined> {
-        return this.kv.get(guildId + this.SEPARATOR + k)
+    get(guildId: string, k: string): Promise<unknown | undefined> {
+        let key = guildId + this.SEPARATOR + k;
+        return this._get(key);
     }
 
-    setGlobal(k: string, v: string): Promise<true> {
-        return this.kv.set(k, v)
+    setGlobal(k: string, v: unknown): Promise<true> {
+        return this._set(k, v);
     }
 
-    getGlobal(k: string): Promise<string | undefined> {
-        return this.kv.get(k)
+    getGlobal(k: string): Promise<unknown | undefined> {
+        return this._get(k);
     }
 
-    clear(): Promise<void> {
-        return this.kv.clear()
+    private async _set(k: string, v: unknown): Promise<true> {
+        this.cache.set(k, v);
+        return await this.kv.set(k, v);
+    }
+
+    private async _get(k: string): Promise<unknown | undefined> {
+        if (this.cache.has(k)) {
+            return this.cache.get(k);
+        } else {
+            let v = await this.kv.get(k);
+            if (v === undefined) return undefined
+            this.cache.set(k, v);
+            return v;
+        }
+    }
+
+    async clear(): Promise<void> {
+        return this.kv.clear();
     }
 }
 
