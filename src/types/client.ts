77648207e7481eb1e8870e85860c16a3fd97adb9
ben2613 <sosgyuki@gmail.com>
2022-05-12 21:42:26 +0100

    Add formatter for clean up and fx

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,8 +1,10 @@
 import { Client, Collection } from "discord.js"
 import Database from "../utils/database"
 import { Command } from "./command"
+import Formatter from "../components/formatter"
 
 export type MyClient = Client & {
     commands: Collection<string, Command>,
-    db: Database
+    db: Database,
+    formatter: Formatter
 }
