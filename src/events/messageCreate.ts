5e94180781f2204cbbcdb39c3fed00d70ee9dfc0
ben2613 <sosgyuki@gmail.com>
2022-06-01 18:07:53 +0100

    possible_sensitive is not reliable, change back to timeout

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -43,44 +43,44 @@ module.exports = {
         if (msg.content.includes("https://twitter.com/")) {
             // do not delete original message
             // extract visible tweet urls
-            let tweetUrls = mc.formatter.extractVisibleTweetURLs(msg.content);
-            let tie = new TweetMediaEmbed(mc.twitterClient, mc.db);
-            let filterRes = await Promise.all(tweetUrls.map(tie.getPossibleSensitive, tie));
-            tweetUrls = tweetUrls.filter((v, i) => filterRes[i]);
-            if (tweetUrls.length !== 0) {
-                let embedsToSend = await tie.getEmbedsFromUrls(tweetUrls);
-                for (let i = 0; i < embedsToSend.length; i++) {
-                    let reply: Message<boolean>;
-                    const e = embedsToSend[i];
-                    if (e.url) { // mp4
-                        reply = await msg.reply({
-                            content: mc.formatter.hideAllLinkEmbed(
-                                e.embed.author?.name + "\n" + e.embed.description).split("\n").map(quote).join("\n")
-                                + "\n" + e.url,
-                            allowedMentions: {
-                                repliedUser: false,
-                            }
-                        });
-                    } else {
-                        reply = await msg.reply({
-                            embeds: [e.embed],
-                            allowedMentions: {
-                                repliedUser: false,
-                            }
-                        });
-                    }
-                    const boki = msg.client.emojis.cache.find(emoji => emoji.name !== null && emoji.name.includes('Boki'))
-                    if (boki) {
-                        await reply.react(boki)
-                    }
-                    await reply.react('❌')
-                    if (e.embed.footer?.text.match(/[234]/)) {
-                        reply.react('◀️').then(() => {
-                            reply.react('▶️')
-                        })
+            setTimeout(async () => {
+                let tweetUrls = mc.formatter.extractUnembeddedTweetURLs(msg.content, msg.embeds);
+                let tie = new TweetMediaEmbed(mc.twitterClient, mc.db);
+                if (tweetUrls.length !== 0) {
+                    let embedsToSend = await tie.getEmbedsFromUrls(tweetUrls);
+                    for (let i = 0; i < embedsToSend.length; i++) {
+                        let reply: Message<boolean>;
+                        const e = embedsToSend[i];
+                        if (e.url) { // mp4
+                            reply = await msg.reply({
+                                content: mc.formatter.hideAllLinkEmbed(
+                                    e.embed.author?.name + "\n" + e.embed.description).split("\n").map(quote).join("\n")
+                                    + "\n" + e.url,
+                                allowedMentions: {
+                                    repliedUser: false,
+                                }
+                            });
+                        } else {
+                            reply = await msg.reply({
+                                embeds: [e.embed],
+                                allowedMentions: {
+                                    repliedUser: false,
+                                }
+                            });
+                        }
+                        const boki = msg.client.emojis.cache.find(emoji => emoji.name !== null && emoji.name.includes('Boki'))
+                        if (boki) {
+                            await reply.react(boki)
+                        }
+                        await reply.react('❌')
+                        if (e.embed.footer?.text.match(/[234]/)) {
+                            reply.react('◀️').then(() => {
+                                reply.react('▶️')
+                            })
+                        }
                     }
                 }
-            }
+            })
         }
     }
 }
